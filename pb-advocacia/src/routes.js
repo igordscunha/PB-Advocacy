45c5493193144981c093a3ffc25c8945ef457f25
igordscunha <124375584+igordscunha@users.noreply.github.com>
2023-05-03 17:03:37 -0300

    Update routes.js

--- pb-advocacia/src/routes.js
+++ pb-advocacia/src/routes.js
@@ -5,6 +5,7 @@ import { AreasAtuacao } from 'Pages/AreasAtuacao'
 import { Contato } from 'Pages/Contato'
 import { Banner } from 'Components/Banner'
 import ScrollToTop from 'Components/ScrollToTop'
+import { NotFound } from 'Components/NotFound'
 
 export const AppRouter = () => {
     return(
