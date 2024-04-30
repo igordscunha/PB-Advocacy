41f9af5dd6421d0db51dd4f9d4ea9cb79afe5572
Igor Cunha <igordscunha@outlook.com>
2024-04-29 21:35:55 -0300

    consertando imagens

--- src/Pages/Home/AreasAtuacao/index.tsx
+++ src/Pages/Home/AreasAtuacao/index.tsx
@@ -6,27 +6,27 @@ export const AreasAtuacao = () => {
             </div>
             <div className="md:flex md:gap-16 md:justify-center md:py-24">
                 <div className="flex flex-col items-center md:cursor-pointer md:hover:scale-125 md:transition-transform">
-                    <img className="rounded-3xl shadow-xl mt-16 w-1/3 md:w-full" src='/public/images/civil.png' alt='civil'/>
+                    <img className="rounded-3xl shadow-xl mt-16 w-1/3 md:w-full" src='../../../../public/images/civil.png' alt='civil'/>
                     <h4 className="text-2xl text-white pt-4 uppercase">Civil</h4>
                 </div>
 
                 <div className="flex flex-col items-center md:cursor-pointer md:hover:scale-125 md:transition-transform">
-                    <img className="rounded-3xl shadow-xl mt-16 w-1/3 md:w-full" src='/public/images/trabalhista.png' alt='trabalhista'/>
+                    <img className="rounded-3xl shadow-xl mt-16 w-1/3 md:w-full" src='../../../../public/images/trabalhista.png' alt='trabalhista'/>
                     <h4 className="text-2xl text-white pt-4 uppercase">Trabalhista</h4>
                 </div>
 
                 <div className="flex flex-col items-center md:cursor-pointer md:hover:scale-125 md:transition-transform">
-                    <img className="rounded-3xl shadow-xl mt-16 w-1/3 md:w-full" src='/public/images/digital.png' alt='digital'/>
+                    <img className="rounded-3xl shadow-xl mt-16 w-1/3 md:w-full" src='../../../../public/images/digital.png' alt='digital'/>
                     <h4 className="text-2xl text-white pt-4 uppercase">Digital</h4>
                 </div>
 
                 <div className="flex flex-col items-center md:cursor-pointer md:hover:scale-125 md:transition-transform">
-                    <img className="rounded-3xl shadow-xl mt-16 w-1/3 md:w-full" src='/public/images/empresarial.png' alt='empresarial'/>
+                    <img className="rounded-3xl shadow-xl mt-16 w-1/3 md:w-full" src='../../../../public/images/empresarial.png' alt='empresarial'/>
                     <h4 className="text-2xl text-white pt-4 uppercase">Empresarial</h4>
                 </div>
 
                 <div className="flex flex-col items-center md:cursor-pointer md:hover:scale-125 md:transition-transform">
-                    <img className="rounded-3xl shadow-xl mt-16 w-1/3 md:w-full" src='/public/images/internacional.png' alt='internacional'/>
+                    <img className="rounded-3xl shadow-xl mt-16 w-1/3 md:w-full" src='../../../../public/images/internacional.png' alt='internacional'/>
                     <h4 className="text-2xl text-white pt-4 uppercase">Internacional</h4>
                 </div>
             </div>
--- src/Pages/Home/Redes Sociais/index.tsx
+++ src/Pages/Home/Redes Sociais/index.tsx
@@ -12,18 +12,18 @@ export const RedesSociais = () => {
             </div>
 
             <div className="flex flex-col gap-8 items-center md:hidden">
-                <img className="rounded-2xl shadow-xl w-1/2 cursor-pointer" src='/public/images/redesocial5.png' alt='rede social1'/>
-                <img className="rounded-2xl shadow-xl w-1/2 cursor-pointer" src='/public/images/redesocial3.png' alt='rede social2'/>
-                <img className="rounded-2xl shadow-xl w-1/2 cursor-pointer" src='/public/images/redesocial6.png' alt='rede social3'/>
+                <img className="rounded-2xl shadow-xl w-1/2 cursor-pointer" src='../../../../public/images/redesocial5.png' alt='rede social1'/>
+                <img className="rounded-2xl shadow-xl w-1/2 cursor-pointer" src='../../../../public/images/redesocial3.png' alt='rede social2'/>
+                <img className="rounded-2xl shadow-xl w-1/2 cursor-pointer" src='../../../../public/images/redesocial6.png' alt='rede social3'/>
             </div>
 
             <div className="hidden md:grid grid-cols-3 grid-rows-2 justify-items-center gap-12 md:pb-24">
-                <img className="rounded-2xl shadow-xl cursor-pointer md:hover:translate-y-2 md:transition-transform" src='/public/images/redesocial3.png' alt='rede social2'/>
-                <img className="rounded-2xl shadow-xl cursor-pointer md:hover:translate-y-2 md:transition-transform" src='/public/images/redesocial6.png' alt='rede social3'/>
-                <img className="rounded-2xl shadow-xl cursor-pointer md:hover:translate-y-2 md:transition-transform" src='/public/images/redesocial2.png' alt='rede social4'/>
-                <img className="rounded-2xl shadow-xl cursor-pointer md:hover:-translate-y-2 md:transition-transform" src='/public/images/redesocial4.png' alt='rede social5'/>
-                <img className="rounded-2xl shadow-xl cursor-pointer md:hover:-translate-y-2 md:transition-transform" src='/public/images/redesocial5.png' alt='rede social1'/>
-                <img className="rounded-2xl shadow-xl cursor-pointer md:hover:-translate-y-2 md:transition-transform" src='/public/images/redesocial1.png' alt='rede social6'/>
+                <img className="rounded-2xl shadow-xl cursor-pointer md:hover:translate-y-2 md:transition-transform" src='../../../../public/images/redesocial3.png' alt='rede social2'/>
+                <img className="rounded-2xl shadow-xl cursor-pointer md:hover:translate-y-2 md:transition-transform" src='../../../../public/images/redesocial6.png' alt='rede social3'/>
+                <img className="rounded-2xl shadow-xl cursor-pointer md:hover:translate-y-2 md:transition-transform" src='../../../../public/images/redesocial2.png' alt='rede social4'/>
+                <img className="rounded-2xl shadow-xl cursor-pointer md:hover:-translate-y-2 md:transition-transform" src='../../../../public/images/redesocial4.png' alt='rede social5'/>
+                <img className="rounded-2xl shadow-xl cursor-pointer md:hover:-translate-y-2 md:transition-transform" src='../../../../public/images/redesocial5.png' alt='rede social1'/>
+                <img className="rounded-2xl shadow-xl cursor-pointer md:hover:-translate-y-2 md:transition-transform" src='../../../../public/images/redesocial1.png' alt='rede social6'/>
             </div>
             
         </section>
