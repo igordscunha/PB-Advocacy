c205e671a05c71969d153c2807f722f25adcff5e
Igor Cunha <igordscunha@outlook.com>
2024-04-29 21:15:32 -0300

    ultimas mudanças

--- pb-advocacia/README.md
+++
@@ -1,7 +0,0 @@
-# P&B Advocacia by Igor Cunha
-
-Replica of a responsive website for a law firm made by me.
-
-## ReactJS and TypeScript.
-
-### igordscunha@outlook.com
--- pb-advocacia/src/Components/Linha/index.tsx
+++
@@ -1,7 +0,0 @@
-export const Linha = () => {
-    return(
-        <div className="flex justify-center pt-8 md:py-24">
-            <div className="w-full border-b-2 border-amber-300"></div>
-        </div>
-    )
-}
